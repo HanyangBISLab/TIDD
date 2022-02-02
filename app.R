7e3a2af3118c08508239013d8ae6b9f356a4c07e
HonglanLi <cy0218@gmail.com>
2022-02-03 07:31:58 +0900

    Update app.R

--- app.R
+++ app.R
@@ -1,3 +1,25 @@
-# Run the application
+#   TIDD: peptide rescoring s/w
+#
+#   Written by H. Li <hllee@hanyang.ac.kr>
+#
+#   Copyright (C) 2022 BIS Labs, Hanyang Univ. Korea
+#
+#   TIDD is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   TIDD is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with TIDD.  If not, see <http://www.gnu.org/licenses/>.
 
-shinyApp(ui = ui, server = server, display.mode="showcase")
+
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
